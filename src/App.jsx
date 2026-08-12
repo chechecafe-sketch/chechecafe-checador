c31f37e92e5c6242e0b63a2cbd3555b56cb9fd6d
Luis Garinian <entorno@Luiss-MacBook-Pro-2.local>
2026-08-12 13:36:23 -0600

    coordenadas correctas tienda Centro

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const STORES = [
   { id: "SMR", name: "Santa María La Ribera", lat: 19.44815114965266, lng: -99.15418204004736 },
   { id: "TAB", name: "Tabacalera", lat: 19.43910242690003, lng: -99.15578818924966 },
   { id: "JUA", name: "Juárez", lat: 19.426522070418077, lng: -99.15542057575757 },
-  { id: "CEN", name: "Centro", lat: 19.435325031610592, lng: -99.13280633342903 },
+  { id: "CEN", name: "Centro", lat: 19.4351227, lng: -99.1327956 },
   { id: "JAR", name: "Jardín", lat: 19.45033734600347, lng: -99.16094680274179 },
   { id: "DVA", name: "Del Valle", lat: 19.373600044458726, lng: -99.16078647575902 },
 ];
